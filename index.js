78005834a032333012a2702d185cbcffdd7fec24
DanielOmehe <omehedaniel232@gmail.com>
2024-09-29 22:55:23 +0100

    Made some changes to make page dynamic

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(window.clientWidth);
+const shareBtn = document.querySelector(".article-share-btn");
+const articleShare = document.querySelector(".article-share");
+const closeShareMobile = document.querySelector('.article-share-close');
+
+shareBtn.addEventListener("mouseover", (e) => {
+  shareBtn.classList.add("hover");
+  shareBtn.innerHTML = `<img src="images/icon-share-white.svg" alt="share-icon" />`;
+});
+
+shareBtn.addEventListener("mouseout", (e) => {
+  shareBtn.classList.remove("hover");
+  shareBtn.innerHTML = `<img src="images/icon-share.svg" alt="share-icon" />`;
+});
+
+shareBtn.addEventListener("click", (e)=>{
+    articleShare.classList.toggle("open");
+});
+
+closeShareMobile.addEventListener('click', (e)=>{
+    articleShare.classList.remove('open');
+})
